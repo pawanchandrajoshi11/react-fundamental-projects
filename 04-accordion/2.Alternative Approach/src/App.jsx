13ba462c9bfb658365f6163c8e85b21664266ba5
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-09 13:11:09 +0530

    Setup (2.Alternative Approach) folder

--- 04-accordion/2.Alternative Approach/src/App.jsx
+++ 04-accordion/2.Alternative Approach/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react';
-import data from './data';
-import Questions from './Questions';
+import { useState } from "react";
+import data from "./data";
+import Questions from "./Questions";
 
-function App() {
+const App = () => {
   const [questions, setQuestions] = useState(data);
   return (
     <main>
       <Questions questions={questions} />
     </main>
   );
-}
-
+};
 export default App;
--- 04-accordion/2.Alternative Approach/src/SingleQuestion.jsx
+++ 04-accordion/2.Alternative Approach/src/SingleQuestion.jsx
@@ -1,18 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+import { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
 const SingleQuestion = ({ title, info }) => {
   const [showInfo, setShowInfo] = useState(false);
+
   return (
-    <article className='question'>
+    <article className="question">
       <header>
         <h5>{title}</h5>
-        <button className='question-btn' onClick={() => setShowInfo(!showInfo)}>
+        <button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
           {showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
         </button>
       </header>
-      {showInfo && <p>{info}</p>}
+      <p>{showInfo && info}</p>
     </article>
   );
 };
-
 export default SingleQuestion;
