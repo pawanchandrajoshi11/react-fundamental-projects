d5e9a4c3dd61671ea998840802e42ac5d958ac00
Pawan Chandra Joshi <pawanpcj600@gmail.com>
2023-11-25 13:03:57 +0530

    Final Commit 08-lorem-ipsum

--- 08-lorem-ipsum/starter/src/App.jsx
+++ 08-lorem-ipsum/starter/src/App.jsx
@@ -1,4 +1,39 @@
+import { useState } from "react";
+import data from "./data";
+import { v4 as uuid } from "uuid";
+
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const intCount = parseInt(count);
+    setText(data.slice(0, intCount));
+  };
+  return (
+    <section className="section-center">
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">Paragraph:</label>
+        <input
+          type="number"
+          name="amount"
+          id="amount"
+          min={1}
+          max={8}
+          step={1}
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button className="btn">Generate</button>
+      </form>
+      <article className="lorem-text">
+        {text.map((para) => {
+          return <p key={uuid()}>{para}</p>;
+        })}
+      </article>
+    </section>
+  );
 };
 export default App;
